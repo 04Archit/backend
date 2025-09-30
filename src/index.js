875182b8d657fbd84ceeb1f4cf4b514b64882b94
Archit <architpathak8261@gmail.com>
2025-09-30 19:31:09 +0530

    databases is connected

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
+// require("dotenv").config({ path: "./env" });
+
+import dotenv from "dotenv";
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants.js";
+import { connectDB } from "../src/db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+connectDB();
+
+// import express from "express";
+// const app = express();
 
 // (async () => {
 //   try {
 //     await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+//     app.on("error", (error) => {
+//       console.log("err: ", error);
+//     });
+
+//     app.listen(process.env.PORT, () => {
+//       console.log(`App is listening on port ${process.env.PORT}`);
+//     });
 //   } catch (err) {
 //     console.error("ERROR: ", err);
 //   }
