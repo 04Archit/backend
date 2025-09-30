0cc9e595f0801311d1c343c31504c2e7d28efb3e
Archit <architpathak8261@gmail.com>
2025-09-30 16:29:00 +0530

    database connectivity

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+  } catch (err) {
+    console.error("ERROR: ", err);
+  }
+})();
