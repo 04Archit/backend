4751d414ef407fff531d1852ea519c75f15231a2
Archit <architpathak8261@gmail.com>
2025-09-30 16:38:13 +0530

    comment

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants";
 
-(async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
-  } catch (err) {
-    console.error("ERROR: ", err);
-  }
-})();
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+//   } catch (err) {
+//     console.error("ERROR: ", err);
+//   }
+// })();
